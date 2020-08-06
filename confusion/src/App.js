59693f17894c3c094916c4c836faa7439eb44b9f
Pedro Lopes Maia <pedrolopes_maia@hotmail.com>
2020-08-06 12:18:11 -0300

    Components Part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,8 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponents";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +22,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
